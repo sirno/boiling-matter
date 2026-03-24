efac995a05664f7a22a6b2d87d55f80e2d52f559
Nicolas Ochsner <nicolasochsner@gmail.com>
2026-03-24 14:58:29 +0100

    Add ema voltage sensor smoothing

--- main/app_driver.cpp
+++ main/app_driver.cpp
@@ -20,6 +20,9 @@ static const char *TAG = "app_driver";
 #define ADC_ATTEN ADC_ATTEN_DB_12
 #define ADC1_CHAN0 ADC_CHANNEL_0
 
+#define ADC_NUM_SAMPLES 5
+#define ADC_EMA_ALPHA 0.2f
+
 static size_t n_sensors;
 static onewire_addr_t sensor_addrs[ONEWIRE_MAX_DS18B20];
 
@@ -27,8 +30,10 @@ static adc_oneshot_unit_handle_t adc_handle;
 static adc_cali_handle_t voltage_handle;
 static esp_timer_handle_t timer;
 
+#ifdef CONFIG_USE_WINDOW_FOR_VOLTAGE_SMOOTHING
 static float voltage_window[VOLTAGE_SMOOTHING_WINDOW_SIZE] = {0};
 static int voltage_window_index = 0;
+
 
 static float smooth_voltage(float voltage) {
   voltage_window[voltage_window_index] = voltage;
@@ -49,6 +54,29 @@ static float smooth_voltage(float voltage) {
 
   return sum / count;
 }
+#endif
+
+#ifdef CONFIG_USE_EMA_FOR_VOLTAGE_SMOOTHING
+static float ema_voltage = 0;
+
+static float smooth_voltage(float voltage) {
+  if (ema_voltage <= 0) {
+    ema_voltage = voltage;
+    return voltage;
+  }
+
+  ema_voltage = ADC_EMA_ALPHA * voltage + (1 - ADC_EMA_ALPHA) * ema_voltage;
+  return ema_voltage;
+}
+#endif
+
+#ifndef CONFIG_USE_WINDOW_FOR_VOLTAGE_SMOOTHING
+#ifndef CONFIG_USE_EMA_FOR_VOLTAGE_SMOOTHING
+static float smooth_voltage(float voltage) {
+  return voltage;
+}
+#endif
+#endif
 
 static esp_err_t adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle)
 {
@@ -151,12 +179,20 @@ static void timer_cb_internal(void *arg) {
   for (int i = 0; i < n_sensors; i++) {
     ctx->temperature.cb(ctx->temperature.endpoint_id, temperatures[i], NULL);
   }
-  int adc_raw;
-  int voltage;
 
-  ESP_ERROR_CHECK(adc_oneshot_read(adc_handle, ADC1_CHAN0, &adc_raw));
-  ESP_ERROR_CHECK(adc_cali_raw_to_voltage(voltage_handle, adc_raw, &voltage));
+  int cummulative_voltage = 0;
 
+  for (int i = 0; i < ADC_NUM_SAMPLES; i++) {
+    int adc_raw;
+    int voltage;
+
+    ESP_ERROR_CHECK(adc_oneshot_read(adc_handle, ADC1_CHAN0, &adc_raw));
+    ESP_ERROR_CHECK(adc_cali_raw_to_voltage(voltage_handle, adc_raw, &voltage));
+
+    cummulative_voltage += voltage;
+  };
+
+  int voltage = cummulative_voltage / ADC_NUM_SAMPLES;
   ctx->batvoltage.cb(ctx->batvoltage.endpoint_id, smooth_voltage(2.0 * voltage), NULL);
 }
 
